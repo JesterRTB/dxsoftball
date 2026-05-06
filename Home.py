3c3710830df0c3d57a6adb2c378f6b6d52155d8d
JesterRTB <77212466+JesterRTB@users.noreply.github.com>
2026-05-06 14:56:11 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,1 +1,14 @@
+import streamlit as st
+import pandas as pd
+from supabase import create_client
+
+# Initialize connection
+@st.cache_resource
+def init_connection():
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_KEY"]
+    return create_client(url, key)
+
+supabase = init_connection()
+
 st.image("https://images.seeklogo.com/logo-png/27/1/d-generation-x-logo-png_seeklogo-275249.png")
