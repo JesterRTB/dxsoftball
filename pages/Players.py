e595508a7fcb5f0c90dcd03c1d3703c3721ea2aa
JesterRTB <77212466+JesterRTB@users.noreply.github.com>
2026-05-07 10:03:16 -0500

    Update Players.py

--- pages/Players.py
+++ pages/Players.py
@@ -48,7 +48,7 @@ if selected_player:
     total_row['on_base_plus_slugging'] = total_row['on_base_percentage']+total_row['slugging_percentage']
     total_row['strikeout_percentage']
     total_ops_points = (df['ops_plus']*df['plate_appearances']).sum()
-    #total_woba_points = (df['woba']*df['plate_appearances']).sum()
+    total_woba_points = (df['woba']*df['plate_appearances']).sum()
     total_wrc_points = (df['wrc_plus']*df['plate_appearances']).sum()
     total_pa = df['plate_appearances'].sum()
     total_row['wrc_plus'] = total_wrc_points/total_pa
