8d0ee305d0db589c67904995851ec5f8ba8fc329
JesterRTB <77212466+JesterRTB@users.noreply.github.com>
2026-05-10 01:53:31 -0500

    Update Leaderboard.py

--- pages/Leaderboard.py
+++ pages/Leaderboard.py
@@ -1,1 +1,7 @@
+import streamlit as st
+import pandas as pd
+from Home import supabase
 
+st.set_page_config(page_title="D-X Leaderboard", layout="wide", page_icon="https://images.seeklogo.com/logo-png/27/1/d-generation-x-logo-png_seeklogo-275249.png")
+
+st.subheader(":green[D-Generation X Leaders]")
