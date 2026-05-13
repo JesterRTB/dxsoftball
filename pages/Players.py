f0323485272e1a625a69f4a932e379528a19d23d
JesterRTB <77212466+JesterRTB@users.noreply.github.com>
2026-05-13 10:57:49 -0500

    Update Players.py

--- pages/Players.py
+++ pages/Players.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from Home import supabase
-from utils import get_all_players
+from utils import get_all_players, get_player_seasons
 
 st.set_page_config(page_title="D-X Player Search", layout="wide", page_icon="https://images.seeklogo.com/logo-png/27/1/d-generation-x-logo-png_seeklogo-275249.png")
     
