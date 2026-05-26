b825ba7ef674d6a95f1db5d164ea08319d9f4a1e
JesterRTB <77212466+JesterRTB@users.noreply.github.com>
2026-05-26 11:03:55 -0500

    Update Seasons.py

--- pages/Seasons.py
+++ pages/Seasons.py
@@ -10,4 +10,15 @@ from utils import (
 
 st.set_page_config(page_title="D-X Seasons", layout="wide", page_icon="https://images.seeklogo.com/logo-png/27/1/d-generation-x-logo-png_seeklogo-275249.png")
 
-st.subheader(":green[D-Generation X History]")
+st.header("D-Generation X History")
+
+tab_season_overview, tab_schedule, tab_player_stats = st.tabs(["Season Overview", "Schedule", "Player Stats"])
+
+with tab_season_overview:
+    st.write("Coming soon")
+
+with tab_schedule:
+    st.write("Coming soon")
+
+with tab_player_stats:
+    st.write("Coming soon")
