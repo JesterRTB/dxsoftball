24fec4a33f557a69c3d2e95c43e2ed57badff998
JesterRTB <77212466+JesterRTB@users.noreply.github.com>
2026-05-26 12:27:01 -0500

    Update Seasons.py

--- pages/Seasons.py
+++ pages/Seasons.py
@@ -85,8 +85,8 @@ with tab_season_overview:
             hide_index=True,
             height="content",
             placeholder="",
-            column_order=["season","games","plate_appearances","runs_per_plate_appearance","walk_percentage","strikeout_percentage","batting_average","on_base_percentage","slugging_percentage",
-                         "on_base_plus_slugging","isolated_power","batting_average_balls_in_play","team_calculated_war"],
+            column_order=["season","games","plate_appearances","runs_per_plate_appearance","walk_percentage","strikeout_percentage","extra_base_hit_percentage","batting_average","on_base_percentage",
+                          "slugging_percentage","on_base_plus_slugging","isolated_power","batting_average_balls_in_play","team_calculated_war"],
             column_config={
                 "season": st.column_config.Column("Season", pinned=True, help="**Season**"),
                 "games": st.column_config.NumberColumn("G", format="%d", help="**Games**"),
