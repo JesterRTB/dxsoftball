26e0ffb099dfb168674e3616a6081d2459da547b
JesterRTB <77212466+JesterRTB@users.noreply.github.com>
2026-05-26 11:15:55 -0500

    Update Seasons.py

--- pages/Seasons.py
+++ pages/Seasons.py
@@ -5,7 +5,8 @@ from utils import (
     fetch_player_data,
     get_all_players,
     get_player_seasons,
-    get_sorted_seasons
+    get_sorted_seasons,
+    get_table_data
 )
 
 st.set_page_config(page_title="D-X Seasons", layout="wide", page_icon="https://images.seeklogo.com/logo-png/27/1/d-generation-x-logo-png_seeklogo-275249.png")
@@ -15,7 +16,14 @@ st.header("D-Generation X History")
 tab_season_overview, tab_schedule, tab_player_stats = st.tabs(["Season Overview", "Schedule", "Player Stats"])
 
 with tab_season_overview:
-    st.write("Coming soon")
+    data_season_overview = get_table_data("dx_by_season")
+    df_season_overview = pd.DataFrame(data_season_overview)
+    st.dataframe(
+        df_season_overview,
+        hide_index=True,
+        height="content",
+        column_order=["season","games","wins","losses","ties","win_pct","player_runs","runs_allowed","pythag_win_pct","runs_per_plate_apperance","top_player"]
+    )
 
 with tab_schedule:
     st.write("Coming soon")
