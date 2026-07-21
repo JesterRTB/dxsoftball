b67e306f8ff5c5c5009ebdc8f9b6c097e562728a
JesterRTB <77212466+JesterRTB@users.noreply.github.com>
2026-07-21 11:55:17 -0500

    Update Leaderboard.py

--- pages/Leaderboard.py
+++ pages/Leaderboard.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
-from Home import supabase
-from utils import get_sorted_seasons
+from utils import (
+    get_sorted_seasons,
+    supabase
+)
 
 st.set_page_config(page_title="D-X Leaderboard", layout="wide", page_icon="https://images.seeklogo.com/logo-png/27/1/d-generation-x-logo-png_seeklogo-275249.png")
 
