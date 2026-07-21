25db19daee3a269a330a57a3771f5be3a7c6e1e1
JesterRTB <77212466+JesterRTB@users.noreply.github.com>
2026-07-21 11:52:03 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,58 +1,31 @@
 import streamlit as st
-
 import pandas as pd
-
 from supabase import create_client
-
 import requests
-
 from ics import Calendar
-
 import arrow
 
-
-
 # Initialize connection
-
 @st.cache_resource
-
 def init_connection():
-
     url = st.secrets["SUPABASE_URL"]
-
     key = st.secrets["SUPABASE_KEY"]
-
     return create_client(url, key)
 
-
-
 supabase = init_connection()
-
-
-
 st.set_page_config(page_title="D-Generation X", page_icon="https://images.seeklogo.com/logo-png/27/1/d-generation-x-logo-png_seeklogo-275249.png")
 
-
-
 # 1. Fetch and Parse
-
 def get_league_schedule(url):
-
     # Fetch the raw calendar data
-
     response = requests.get(url)
-
     calendar = Calendar(response.text)
 
     
-
     events = []
-
     for event in calendar.events:
-
         # QuickScores usually provides UTC; convert to Chicago time
         start_time = arrow.get(event.begin).to('US/Central')
-
         events.append({
             "Date": start_time.format('ddd, MMM D'),
             "Time": start_time.format('h:mm A'),
@@ -63,7 +36,6 @@ def get_league_schedule(url):
     
     # Return as a DataFrame sorted by time
     return pd.DataFrame(events).sort_values("Unix")
-
 st.subheader(":green[D-Generation X Schedule]")
 
 try:
