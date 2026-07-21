daa5c236aae21abbb630286807dd9f448f5f8340
JesterRTB <77212466+JesterRTB@users.noreply.github.com>
2026-07-21 11:59:12 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,18 +1,12 @@
 import streamlit as st
 import pandas as pd
-from supabase import create_client
 import requests
 from ics import Calendar
 import arrow
+from utils import (
+    supabase
+)
 
-# Initialize connection
-@st.cache_resource
-def init_connection():
-    url = st.secrets["SUPABASE_URL"]
-    key = st.secrets["SUPABASE_KEY"]
-    return create_client(url, key)
-
-supabase = init_connection()
 st.set_page_config(page_title="D-Generation X", page_icon="https://images.seeklogo.com/logo-png/27/1/d-generation-x-logo-png_seeklogo-275249.png")
 
 # 1. Fetch and Parse
